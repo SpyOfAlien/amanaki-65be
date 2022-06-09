99bc69f5261a4401b9334922e2384d1c36273cc8
dang soan <doanquangbao.it@gmail.com>
2022-06-09 20:51:48 +0700

    Update env saving and loading page

---
+++ src/api/page-loading/controllers/page-loading.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  page-loading controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::page-loading.page-loading');
