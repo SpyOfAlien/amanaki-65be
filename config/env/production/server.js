d951862080fac326d2e8b2060ad9552f7916db11
dang soan <doanquangbao.it@gmail.com>
2022-06-12 22:47:55 +0700

    remove render

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,11 +1,7 @@
-// module.exports = ({ env }) => ({
-//   proxy: true,
-//   url: env('MY_HEROKU_URL'),
-//   app: {
-//     keys: env.array('APP_KEYS')
-//   },
-// })
-
 module.exports = ({ env }) => ({
-  url: env("RENDER_EXTERNAL_URL"),
-});
+  proxy: true,
+  url: env('MY_HEROKU_URL'),
+  app: {
+    keys: env.array('APP_KEYS')
+  },
+})
