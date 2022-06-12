67cd15110f5723a90fe1e62fd62bd529b9c50462
dang soan <doanquangbao.it@gmail.com>
2022-06-12 16:41:03 +0700

    add render yaml

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,7 +1,11 @@
+// module.exports = ({ env }) => ({
+//   proxy: true,
+//   url: env('MY_HEROKU_URL'),
+//   app: {
+//     keys: env.array('APP_KEYS')
+//   },
+// })
+
 module.exports = ({ env }) => ({
-  proxy: true,
-  url: env("MY_HEROKU_URL"),
-  app: {
-    keys: env.array("APP_KEYS"),
-  },
+  url: env("RENDER_EXTERNAL_URL"),
 });
