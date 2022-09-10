33478b25f94679df637ee49ef358eb47af60caf9
dang soan <doanquangbao.it@gmail.com>
2022-09-10 17:09:49 +0700

    update configs

--- config/plugins.js
+++ config/plugins.js
@@ -13,13 +13,13 @@ module.exports = ({ env }) => {
         },
       },
     },
-    "users-permissions": {
-      config: {
-        jwt: {
-          expiresIn: "7d",
-        },
-        jwtSecret: env("JWT_SECRET"),
-      },
-    },
+    // "users-permissions": {
+    //   config: {
+    //     jwt: {
+    //       expiresIn: "7d",
+    //     },
+    //     jwtSecret: env("JWT_SECRET"),
+    //   },
+    // },
   };
 };
