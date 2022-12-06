21ba529c0b75acd8f3d916f5f5557b8463ea6a80
SpyOfAlien <doanquangbao.it@gmail.com>
2022-12-06 14:53:51 +0700

    add rooms

---
+++ src/api/room-list/controllers/room-list.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  room-list controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::room-list.room-list');
---
+++ src/api/room-list/routes/room-list.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * room-list router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::room-list.room-list');
